6105a8981a32dd88a2e6759a3ea2a09f043cd5a3
kieuminhtuan1 <kieuminhtuan2003k@gmail.com>
2023-03-11 15:03:04 +0700

    commit2

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import express from "express";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB"
 require("dotenv").config();
 
 let app = express();
@@ -9,15 +10,17 @@ let app = express();
 //config app
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngine(app);
 initWebRoutes(app);
 
+connectDB();
+
 let port = process.env.PORT || 6969;
 //Port === undefined => port = 6969
 
-app.listen(port, () =>{
+app.listen(port, () => {
     //calback
-    console.log("Backend Nodejs is running on the port:" +port)
+    console.log("Backend Nodejs is running on the port:" + port)
 })
